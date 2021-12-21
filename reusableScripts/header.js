6e8cb1c9106d1338488578e18dacdffa1435168c
hakokoha <kriskosi123@gmail.com>
2021-12-21 13:34:41 +0200

    single service page added

--- reusableScripts/header.js
+++ reusableScripts/header.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
   $(".custom-nav-link").click(function () {
     $($(this).find($(".dropdown-link-container"))[0]).toggle();
-    //$("#about-dropdown").toggle();
   });
   $("#burger-menu-icon").click(function () {
-    console.log("asd");
     $(".navbar").toggle();
   });
 });
