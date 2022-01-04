b1c08cfa81646bdb365384a2916a4c2834354ba6
hakokoha <kriskosi123@gmail.com>
2022-01-04 16:47:01 +0200

    FAQsPage animations added

--- FAQsPage/FAQsPage.js
+++ FAQsPage/FAQsPage.js
@@ -10,5 +10,28 @@ $(document).ready(function () {
 
     $(this.parentNode).find(".togglable-box").toggle();
   });
-  console.log("ready!");
+
+  var controller = new ScrollMagic.Controller();
+
+  for (let i = 1; i <= 3; i++) {
+    new ScrollMagic.Scene({
+      triggerElement: `#fadein-elem${i}`,
+      triggerHook: 0.9, // show, when scrolled 10% into view
+      offset: 50, // move trigger to center of element
+    })
+      .setClassToggle(`#fadein-elem${i}`, "fadein-animation")
+      .reverse(false) // add class to reveal
+      //.addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
+  }
+
+  new ScrollMagic.Scene({
+    triggerElement: `#slide-right-elem1`,
+    triggerHook: 0.9, // show, when scrolled 10% into view
+    offset: 50, // move trigger to center of element
+  })
+    .setClassToggle(`#slide-right-elem1`, "slide-right-animation")
+    .reverse(false) // add class to reveal
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
 });
